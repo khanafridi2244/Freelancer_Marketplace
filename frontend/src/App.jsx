4c3f0194d545eacba0254f25173217163a553353
sikandar <sikanderafridi.854@gmail.com>
2026-07-22 16:25:43 +0500

    setup for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">Freelance Marketplace</h1>
+    </div>
+  )
+}
+
+export default App
